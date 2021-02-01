5b5d6109853a96e60023dfb849f78a794df43c27
dutao <wangyi_dutao@163.com>
2021-02-01 18:19:14 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import * as xxhash from 'xxhash'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const xx64 = xxhash.XXHash64
 createApp(App).use(store).use(router).mount('#app')
